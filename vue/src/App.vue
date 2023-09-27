e3e79e46cf198bef5e93b8d8420ed205468487c2
kayltin <kkkxlin@163.com>
2023-09-27 15:02:00 +0800

    second commit

---
+++ vue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <!--头部区域-->
+    <div style="height: 60px;line-height: 60px;background-color:white;margin-bottom: 2px">
+      <img src="@/assets/book.png" alt="" style="width: 40px;position: relative;top: 10px;left:20px">
+      <span style="margin-left: 30px; font-size: 24px">图书管理系统</span>
+    </div>
+
+    <!--侧边栏和主体-->
+    <div style="display: flex">
+      <!--侧边栏-->
+      <div style="width: 200px;min-height:calc(100vh - 62px); overflow: hidden;margin-right: 2px;background-color:white;">
+        <el-menu :default-active="$route.path" :default-openeds="['/']" router class="el-menu-demo">
+          <!--通过路由router的方式进行页面切换-->
+          <el-menu-item index="/">
+            <i class="el-icon-s-home"></i>
+            <span>首页</span>
+          </el-menu-item>
+          <el-submenu index="/">
+            <template slot="title">
+              <i class="el-icon-s-promotion"></i>
+              <span>关于页面</span>
+            </template>
+            <el-menu-item index="/about">
+              <i class="el-icon-question"></i>
+              <span>关于详情</span>
+            </el-menu-item>
+          </el-submenu>
+
+        </el-menu>
+      </div>
+
+      <!--主体-->
+      <div style="flex: 1;background-color:white;background-color:white;padding: 10px;">
+        <router-view/>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <!--搜索表单-->
+    <div style="margin-bottom: 20px">
+      <el-input style="width: 240px" placeholder="请输入名称"></el-input>
+      <el-input style="margin-left:5px; width: 240px" placeholder="请输入联系方式"></el-input>
+      <el-button style="margin-left: 5px" type="primary">
+        <i class=" el-icon-search"></i>
+        <span>搜索</span>
+      </el-button>
+    </div>
+    <!--stripe显示斑马纹-->
+    <el-table :data="tableData" stripe>
+      <el-table-column prop="name" label="名称"></el-table-column>
+      <el-table-column prop="age" label="年龄"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column prop="phone" label="联系方式"></el-table-column>
+      <el-table-column prop="sex" label="性别"></el-table-column>
+    </el-table>
+
+    <div style="margin-top: 20px">
+      <el-pagination
+          background
+          :page-size="5"
+          layout="prev, pager, next"
+          :total="100">
+      </el-pagination>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+    data(){
+      return{
+        tableData:[
+          {name:'王楚钦',age:23,address:'北京市',phone: '131',sex:'男'},
+          {name:'王楚钦',age:23,address:'北京市',phone: '136623412',sex:'男'},
+          {name:'王楚钦',age:23,address:'北京市',phone: '136623412',sex:'男'},
+          {name:'王楚钦',age:23,address:'北京市',phone: '136623412',sex:'男'},
+        ]
+      }
+    }
+}
+</script>
